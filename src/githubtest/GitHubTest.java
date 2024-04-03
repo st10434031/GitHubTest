4fa0e05a936055c1ca9134549ccfabe4734bfaed
st10434031 <st10434031@vcconnect.edu.za>
2024-04-03 19:45:08 +0200

    my second try at a commit

---
+++ src/githubtest/GitHubTest.java
@@ -0,0 +1,17 @@
+
+package githubtest;
+
+
+public class GitHubTest 
+{
+
+   
+    public static void main(String[] args) 
+    {
+        
+      System.out.println(" Hello World ");
+    
+    }
+    
+    
+}
